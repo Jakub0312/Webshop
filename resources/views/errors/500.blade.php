77be5c9eed3813a764402f1c59cf141cac7c27b6
Jakub Belczyk <9017270@student.zadkine.nl>
2022-02-04 11:44:25 +0100

    500 error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -6,9 +6,9 @@
         <div class="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
             <div class="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
                 <div class="border-t border-gray-200 text-center pt-8">
-                    <h1 class="text-9xl font-bold text-green-400">422</h1>
-                    <h1 class="text-6xl font-medium py-8">Unprocessable entity</h1>
-                    <p class="text-2xl pb-8 px-12 font-medium">The server was unable to process the contained instructions.</p>
+                    <h1 class="text-9xl font-bold text-green-400">500</h1>
+                    <h1 class="text-6xl font-medium py-8">Internal server error</h1>
+                    <p class="text-2xl pb-8 px-12 font-medium">Something has gone wrong on the web site's server.</p>
                     <a href="{{ url('/') }}">
                         <button class="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
                             HOME
@@ -20,4 +20,5 @@
     </div>
 
 @endsection
+
 
