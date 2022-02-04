3ac68df994ce27908d2158ee8cafa5ea52477a28
Joshua Doelman <9010987@student.tcrmbo.nl>
2022-02-04 13:58:10 +0100

    Iedereen kan order plaatsen

--- app/Http/Controllers/open/ProductController.php
+++ app/Http/Controllers/open/ProductController.php
@@ -15,8 +15,10 @@ use App\Models\Product;
 use App\Models\User;
 use Auth;
 use Carbon\Carbon;
+use Hash;
 use Illuminate\Http\Request;
 use Session;
+use Symfony\Component\Console\Input\Input;
 
 class ProductController extends Controller
 {
@@ -85,15 +87,54 @@ class ProductController extends Controller
 
     }
 
-    public function saveOrder(StoreAddressRequest $request)
+    public function saveOrder(Request $request)
     {
-        //$users = User::find(Auth::user()->id);
-        $checkAddress = Address::where('user_id', Auth::user()->id)->first();
-        //dd($checkAddress);
-        if(empty($checkAddress))
-        {
+        if (Auth::check()) { //als er een gebruiker is ingelogd kan de gebruiker worden opgezocht
+            $user = User::find(Auth::user()->id);
+            $checkAddress = Address::where('user_id', $user->id)->first();
+
+            if(empty($checkAddress))
+            {
+                $validated = $request->validate([
+                    'streetname' => 'string|min:5|max:100',
+                    'zipcode' => 'string|min:5|max:15',
+                    'city' => 'string|min:5|max:100',
+                    'country' => 'string|min:5|max:45',
+                    'addresstype' => 'integer',
+                ]);
+                $address = new Address();
+                $address->user_id = $user->id;
+                $address->address = $request->streetname;
+                $address->city = $request->city;
+                $address->zipcode = $request->zipcode;
+                $address->country = $request->country;
+                $address->addresstype_id = $request->input('addresstype');
+                $address->save();
+            }
+        } else {
+
+            $validated = $request->validate([
+                'name' => 'required|string|min:3|max:45',
+                'email' => 'required|string|min:5|max:100|unique:users',
+                'streetname' => 'string|min:5|max:100',
+                'zipcode' => 'string|min:5|max:15',
+                'city' => 'string|min:5|max:100',
+                'country' => 'string|min:5|max:45',
+                'addresstype' => 'integer',
+            ]);
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            if ($request->get('myCheck')) {
+                $validated = $request->validate([
+                    'password' => 'required|min:8|string'
+                ]);
+                $user->password = Hash::make($request->password);
+            }
+            $user->save();
+
             $address = new Address();
-            $address->user_id = Auth::user()->id;
+            $address->user_id = $user->id;
             $address->address = $request->streetname;
             $address->city = $request->city;
             $address->zipcode = $request->zipcode;
@@ -101,12 +142,16 @@ class ProductController extends Controller
             $address->addresstype_id = $request->input('addresstype');
             $address->save();
         }
+
+        //dd($checkAddress);
+
         $cart = Session::get('cart');
         //dd($cart);
 
         $order = new Order();
-        $order->user_id = Auth::user()->id;
+        $order->user_id = $user->id;
         $order->orderdate = Carbon::now();
+        $order->state_id = '1';
         $order->save();
 
         foreach ($cart->items as $value) {
