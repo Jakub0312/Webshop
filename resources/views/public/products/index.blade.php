b83aab15cfe9d6f9152d1a888ad927e0c0eae6b7
Joshua Doelman <9010987@student.tcrmbo.nl>
2022-02-10 14:05:56 +0100

    Product index mooier gemaakt

--- resources/views/public/products/index.blade.php
+++ resources/views/public/products/index.blade.php
@@ -23,15 +23,15 @@
     </h2>
     <div class="flex flex-wrap ml-64 mr-auto">
             @foreach($products as $product)
-                <div class="w-full md:w-1/3 xl:w-1/5 p-6 flex flex-col float-right m-1 border-2 rounded-lg overflow-hidden">
+                <div class="w-full md:w-1/3 xl:w-1/5 p-6 flex flex-col float-right m-1 border-2 rounded-lg ">
                     <a href="#">
                         <img class="hover:grow hover:shadow-lg"
                              src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png">
                         <div class="pt-3 flex items-center justify-between">
                             <h2 class="font-bold text-xl">{{ $product->name }}</h2>
                         </div>
-                        <p class="pt-1 text-gray-900">{{ $product->description }}</p>
-                        <p class="pt-1 text-gray-900 bold text-xl">€{{ $product->latest_price->price }}
+                        <p class="pt-1 text-gray-900 overflow-hidden h-20">{{ $product->description }}</p>
+                        <p class="pt-1 text-gray-900 bold text-xl ">€{{ $product->latest_price->price }}
                             <a href="{{ route('product.addToCart', ['id' => $product->id]) }}">
                         <button class="bg-customgreen hover:bg-customgreen-dark text-white font-bold py-2 px-4 rounded float-right none text-sm">
                             Add to cart
